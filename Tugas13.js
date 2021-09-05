ac8d791eff73718c1dc8200b34d601a8f33fa0f6
susila tropika <64695610+susilaattari@users.noreply.github.com>
2021-09-06 04:52:34 +0700

    Tugas 13 js Niomic

---
+++ Tugas13.js
@@ -0,0 +1,9 @@
+function arrayUnShift(){
+    let makanan = ['somay','otak - otak','sosis bakar','nuget'];
+    console.log(makanan)
+    makanan.unshift('bakso','seblak')
+
+    return makanan
+}
+
+console.log(arrayUnShift())
